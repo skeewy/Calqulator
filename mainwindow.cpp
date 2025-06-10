596f756398c15f54dd5b663b2e94d9519fc1da67
Skeewy <mtahaxd1@gmail.com>
2025-06-10 01:41:35 +0100

    Describe what you changed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <vector>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,12 +14,41 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-struct calc
-{
-};
-
 void MainWindow::on_equalButton_clicked()
 {
     QString operation = ui->Operation->text();
+    std::vector<double> nums;
+    int start = 0;
+    int numsI = 0;
+
+    for (int i = 0; i < operation.length(); i++)
+    {
+        switch (operation[i].unicode())
+        {
+            case '+':
+                start = i+1;
+                numsI++;
+
+                nums.push_back((operation.mid(start, i)).toDouble());
+                break;
+            case '-':
+                start = i+1;
+                numsI++;
+
+                break;
+            case '*':
+                start = i+1;
+                numsI++;
+
+                break;
+            case '/':
+                start = i+1;
+                numsI++;
+
+                break;
+            default:
+                break;
+        }
+    }
 }
 
